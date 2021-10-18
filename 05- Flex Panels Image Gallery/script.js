9c0f00c7796bab464dcfbceca20234804dc64e24
abhipshapatro <71815946+abhipshapatro@users.noreply.github.com>
2021-10-18 13:57:25 +0530

    Flex Panel Image Gallery

---
+++ 05- Flex Panels Image Gallery/script.js
@@ -0,0 +1,16 @@
+const panels = document.querySelectorAll('.panel');
+
+function toggleOpen() {
+    console.log("hello");
+    this.classList.toggle('open');
+}
+
+function toggleActive(e) {
+    console.log(e.propertyName); //tells which property is working
+    if (e.propertyName.includes('flex')) {
+        this.classList.toggle('open-active');
+    }
+}
+
+panels.forEach(panel => panel.addEventListener('click' , toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend' , toggleActive));
